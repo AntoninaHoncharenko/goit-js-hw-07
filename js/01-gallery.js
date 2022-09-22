00a8fbbab715a2c2ecfd5e815be280a955361c8f
AntoninaHoncharenko <g.antonina.vl@gmail.com>
2022-09-22 14:56:17 +0300

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+// + 1. Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+
+const galeryRef = document.querySelector(".gallery");
+
+const galeryEl = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`
+  )
+  .join("");
+
+galeryRef.insertAdjacentHTML("beforeend", galeryEl);
+
+// 2. Реалізація делегування на div.gallery і отримання url великого зображення.
+
+galeryRef.addEventListener("click", onImageClick);
+
+function onImageClick(event) {
+  event.preventDefault();
+  const modal = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+  modal.show();
+}
+
+// + 3. Підключення скрипту і стилів бібліотеки модального вікна basicLightbox.
+// Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані(.min) файли бібліотеки.
+
+// + 4. Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
+
+// + 5. Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям.
+// Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
